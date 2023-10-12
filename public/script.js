19add296251b56376d673a6dae225e114e58b106
sokiicz <zavadilj@email.cz>
2023-10-12 17:39:51 +0200

    Warning before refreshing, count is as a loss6

--- public/script.js
+++ public/script.js
@@ -237,7 +237,9 @@ function selectTeam(team) {
 let isGameRunning = false;
 
 window.addEventListener("beforeunload", function (e) {
-    var confirmationMessage = "Are you sure you want to leave the game?";
+    updateLossCount();
+    var confirmationMessage = "If you refresh or leave, it will be counted as a loss. Are you sure?";
     e.returnValue = confirmationMessage;
     return confirmationMessage;
 });
+
